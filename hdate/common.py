32e36826ec8d5c71fadd2e501dd57fa6015aab8c
Tsvi Mostovicz <ttmost@gmail.com>
2018-12-13 12:37:09 +0200

    Make API more comparable to astral API
    We want to get more accurate results by using the astral library. To that end, I've
    reworked the API to be more similar to astral.
    
    This has been done by pulling out the time calculations from Zmanim and moving them
    to the location object.
    
    Two notes that have come up:
    
      1. Should we move diaspora back from Location to Zmanim?
      2. The current implementation causes all the calculations to be done every time we
         try accessing a single item from the zmanim dictionary. A better way might be to
         create the calculations of sunrise, sunset etc as a separate property, and have
         zmanim dictionary only access the necessary calculations.

--- hdate/common.py
+++ hdate/common.py
@@ -1,11 +1,15 @@
 """Small helper classes."""
 
-import datetime
+import datetime as dt
+import logging
+import math
 import sys
 
 import pytz
 
 from hdate.htables import Months
+_LOGGER = logging.getLogger(__name__)
+HebrewDate = namedtuple("HebrewDate", ["year", "month", "day"])
 
 
 class BaseClass(object):  # pylint: disable=useless-object-inheritance
@@ -46,6 +50,9 @@ class HebrewDate(BaseClass):  # pylint: disable=too-few-public-methods
 
 class Location(BaseClass):
     """Define a geolocation for Zmanim calculations."""
+
+    SUN_RISING = 0
+    SUN_SETTING = 1
 
     # pylint: disable=too-many-arguments
     def __init__(
@@ -91,3 +98,84 @@ class Location(BaseClass):
         self._timezone = (
             value if isinstance(value, datetime.tzinfo) else pytz.timezone(value)
         )
+
+    def time_at_elevation(self, elevation, direction=SUN_RISING, date=None,
+                          local=True):
+        _LOGGER.debug("Calculating time at elevation: %f", elevation)
+        rise_minutes, set_minutes = self._get_utc_sun_time_deg(elevation, date)
+        _LOGGER.debug("Rise time: %d, Set time: %d", rise_minutes, set_minutes)
+        result = rise_minutes if direction == self.SUN_RISING else set_minutes
+        return self.minutes_to_datetime(date, result, local)
+
+    def _get_utc_sun_time_deg(self, deg, date=None):
+        """
+        Return the times in minutes from 00:00 (utc) for a given sun altitude.
+
+        This is done for a given sun altitude in sunrise `deg` degrees
+        This function only works for altitudes sun really is.
+        If the sun never gets to this altitude, the returned sunset and sunrise
+        values will be negative. This can happen in low altitude when latitude
+        is nearing the poles in winter times, the sun never goes very high in
+        the sky there.
+
+        Algorithm from
+        http://www.srrb.noaa.gov/highlights/sunrise/calcdetails.html
+        The low accuracy solar position equations are used.
+        These routines are based on Jean Meeus's book Astronomical Algorithms.
+        """
+        gama = 0        # location of sun in yearly cycle in radians
+        eqtime = 0      # difference betwen sun noon and clock noon
+        decl = 0        # sun declanation
+        hour_angle = 0  # solar hour angle
+        sunrise_angle = math.pi * deg / 180.0  # sun angle at sunrise/set
+
+        # get the day of year
+        day_of_year = (date - dt.date(date.year, 1, 1)).days
+
+        # get radians of sun orbit around earth =)
+        gama = 2.0 * math.pi * ((day_of_year - 1) / 365.0)
+
+        # get the diff betwen suns clock and wall clock in minutes
+        eqtime = 229.18 * (0.000075 + 0.001868 * math.cos(gama) -
+                           0.032077 * math.sin(gama) -
+                           0.014615 * math.cos(2.0 * gama) -
+                           0.040849 * math.sin(2.0 * gama))
+
+        # calculate suns declanation at the equater in radians
+        decl = (0.006918 - 0.399912 * math.cos(gama) +
+                0.070257 * math.sin(gama) -
+                0.006758 * math.cos(2.0 * gama) +
+                0.000907 * math.sin(2.0 * gama) -
+                0.002697 * math.cos(3.0 * gama) +
+                0.00148 * math.sin(3.0 * gama))
+
+        # we use radians, ratio is 2pi/360
+        latitude = math.pi * self.latitude / 180.0
+
+        # the sun real time diff from noon at sunset/rise in radians
+        try:
+            hour_angle = (math.acos(
+                math.cos(sunrise_angle) /
+                (math.cos(latitude) * math.cos(decl)) -
+                math.tan(latitude) * math.tan(decl)))
+        # check for too high altitudes and return negative values
+        except ValueError:
+            return -720, -720
+
+        # we use minutes, ratio is 1440min/2pi
+        hour_angle = 720.0 * hour_angle / math.pi
+
+        # get sunset/rise times in utc wall clock in minutes from 00:00 time
+        # sunrise / sunset
+        return int(720.0 - 4.0 * self.longitude - hour_angle - eqtime), \
+            int(720.0 - 4.0 * self.longitude + hour_angle - eqtime)
+
+    def minutes_to_datetime(self, date, minutes_from_utc, as_local=True):
+        """Return the local time for a given time UTC."""
+        from_zone = tz.gettz('UTC')
+        to_zone = self.timezone
+        utc = dt.datetime.combine(date, dt.time()) + \
+            dt.timedelta(minutes=minutes_from_utc)
+        utc = utc.replace(tzinfo=from_zone)
+        local = utc.astimezone(to_zone)
+        return local if as_local else utc
--- hdate/zmanim.py
+++ hdate/zmanim.py
@@ -15,8 +15,13 @@ import math
 import pytz
 
 from hdate import htables
-from hdate.common import BaseClass, Location
+from hdate.common import BaseClass
 from hdate.date import HDate
+
+try:
+    from astral import Location
+except ImportError:
+    from hdate.common import Location
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -194,115 +199,47 @@ class Zmanim(BaseClass):
 
         return False
 
-    def gday_of_year(self):
-        """Return the number of days since January 1 of the given year."""
-        return (self.date - dt.date(self.date.year, 1, 1)).days
-
-    def _get_utc_sun_time_deg(self, deg):
-        """
-        Return the times in minutes from 00:00 (utc) for a given sun altitude.
-
-        This is done for a given sun altitude in sunrise `deg` degrees
-        This function only works for altitudes sun really is.
-        If the sun never gets to this altitude, the returned sunset and sunrise
-        values will be negative. This can happen in low altitude when latitude
-        is nearing the poles in winter times, the sun never goes very high in
-        the sky there.
-
-        Algorithm from
-        http://www.srrb.noaa.gov/highlights/sunrise/calcdetails.html
-        The low accuracy solar position equations are used.
-        These routines are based on Jean Meeus's book Astronomical Algorithms.
-        """
-        gama = 0  # location of sun in yearly cycle in radians
-        eqtime = 0  # difference betwen sun noon and clock noon
-        decl = 0  # sun declanation
-        hour_angle = 0  # solar hour angle
-        sunrise_angle = math.pi * deg / 180.0  # sun angle at sunrise/set
-
-        # get the day of year
-        day_of_year = self.gday_of_year()
-
-        # get radians of sun orbit around earth =)
-        gama = 2.0 * math.pi * ((day_of_year - 1) / 365.0)
-
-        # get the diff betwen suns clock and wall clock in minutes
-        eqtime = 229.18 * (
-            0.000075
-            + 0.001868 * math.cos(gama)
-            - 0.032077 * math.sin(gama)
-            - 0.014615 * math.cos(2.0 * gama)
-            - 0.040849 * math.sin(2.0 * gama)
-        )
-
-        # calculate suns declanation at the equater in radians
-        decl = (
-            0.006918
-            - 0.399912 * math.cos(gama)
-            + 0.070257 * math.sin(gama)
-            - 0.006758 * math.cos(2.0 * gama)
-            + 0.000907 * math.sin(2.0 * gama)
-            - 0.002697 * math.cos(3.0 * gama)
-            + 0.00148 * math.sin(3.0 * gama)
-        )
-
-        # we use radians, ratio is 2pi/360
-        latitude = math.pi * self.location.latitude / 180.0
-
-        # the sun real time diff from noon at sunset/rise in radians
-        try:
-            hour_angle = math.acos(
-                math.cos(sunrise_angle) / (math.cos(latitude) * math.cos(decl))
-                - math.tan(latitude) * math.tan(decl)
-            )
-        # check for too high altitudes and return negative values
-        except ValueError:
-            return -720, -720
-
-        # we use minutes, ratio is 1440min/2pi
-        hour_angle = 720.0 * hour_angle / math.pi
-
-        # get sunset/rise times in utc wall clock in minutes from 00:00 time
-        # sunrise / sunset
-        longitude = self.location.longitude
-        return (
-            int(720.0 - 4.0 * longitude - hour_angle - eqtime),
-            int(720.0 - 4.0 * longitude + hour_angle - eqtime),
-        )
-
-    def get_utc_sun_time_full(self):
-        """Return a list of Jewish times for the given location."""
+    @property
+    def zmanim(self):
+        """Return a dictionary of the zmanim the object represents."""
         # sunset and rise time
-        sunrise, sunset = self._get_utc_sun_time_deg(90.833)
+        sunrise = self.location.time_at_elevation(
+            90.833, Location.SUN_RISING, self.date)
+        sunset = self.location.time_at_elevation(
+            90.833, Location.SUN_SETTING, self.date)
 
-        # shaa zmanit by gara, 1/12 of light time
-        sun_hour = (sunset - sunrise) // 12
-        midday = (sunset + sunrise) // 2
+        _LOGGER.debug("Total seconds of light: %d",
+                      (sunset - sunrise).total_seconds())
+
+        # shaa zmanit by gra, 1/12 of light time
+        sun_hour = (sunset - sunrise).total_seconds() / 3600 / 12
+        midday = sunrise + ((sunset - sunrise) / 2)
 
         # get times of the different sun angles
-        first_light, _ = self._get_utc_sun_time_deg(106.1)
-        talit, _ = self._get_utc_sun_time_deg(101.0)
-        _, first_stars = self._get_utc_sun_time_deg(96.0)
-        _, three_stars = self._get_utc_sun_time_deg(98.5)
-        mga_sunhour = (midday - first_light) / 6
+        first_light = self.location.time_at_elevation(
+            106.1, Location.SUN_RISING, self.date)
+        talit = self.location.time_at_elevation(
+            101.0, Location.SUN_RISING, self.date)
+        first_stars = self.location.time_at_elevation(
+            96.0, Location.SUN_SETTING, self.date)
+        three_stars = self.location.time_at_elevation(
+            98.5, Location.SUN_SETTING, self.date)
+        mga_sunhour = (midday - first_light).total_seconds() / 3600 / 6
+
+        _LOGGER.debug("Shaa zmanit according to GRA: %f", sun_hour)
+        _LOGGER.debug("Shaa zmanit according to MGA: %f", mga_sunhour)
 
         res = dict(
-            sunrise=sunrise,
-            sunset=sunset,
-            sun_hour=sun_hour,
-            midday=midday,
-            first_light=first_light,
-            talit=talit,
-            first_stars=first_stars,
-            three_stars=three_stars,
-            plag_mincha=sunset - 1.25 * sun_hour,
-            stars_out=sunset + 18.0 * sun_hour / 60.0,
-            small_mincha=sunrise + 9.5 * sun_hour,
-            big_mincha=sunrise + 6.5 * sun_hour,
-            mga_end_shma=first_light + mga_sunhour * 3.0,
-            gra_end_shma=sunrise + sun_hour * 3.0,
-            mga_end_tfila=first_light + mga_sunhour * 4.0,
-            gra_end_tfila=sunrise + sun_hour * 4.0,
-            midnight=midday + 12 * 60.0,
-        )
+            sunrise=sunrise, sunset=sunset, sun_hour=sun_hour,
+            midday=midday, first_light=first_light, talit=talit,
+            first_stars=first_stars, three_stars=three_stars,
+            plag_mincha=sunset - dt.timedelta(hours=1.25 * sun_hour),
+            stars_out=sunset + dt.timedelta(minutes=18. * sun_hour / 60.),
+            small_mincha=sunrise + dt.timedelta(hours=9.5 * sun_hour),
+            big_mincha=sunrise + dt.timedelta(hours=6.5 * sun_hour),
+            mga_end_shma=first_light + dt.timedelta(hours=mga_sunhour * 3.),
+            gra_end_shma=sunrise + dt.timedelta(hours=sun_hour * 3.),
+            mga_end_tfila=first_light + dt.timedelta(hours=mga_sunhour * 4.),
+            gra_end_tfila=sunrise + dt.timedelta(hours=sun_hour * 4.),
+            midnight=midday + dt.timedelta(minutes=12 * 60.))
         return res
